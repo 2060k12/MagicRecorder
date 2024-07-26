15e4135dd19d3c3f3bc9245129868189be16056a
Pranish <iampranish@outlook.com>
2024-07-26 19:14:32 +1000

    Added Login and signup using firebase auth

---
+++ Magic Recorder/Login/LoginRepository.swift
@@ -0,0 +1,8 @@
+//
+//  LoginRepository.swift
+//  Magic Recorder
+//
+//  Created by Pranish Pathak on 26/7/2024.
+//
+
+import Foundation
---
+++ Magic Recorder/Utils/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Magic Recorder
+//
+//  Created by Pranish Pathak on 25/7/2024.
+//
+
+import Foundation
