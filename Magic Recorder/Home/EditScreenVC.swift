a73eccdde1c463e958eec608cf7cf1f085430252
Pranish <iampranish@outlook.com>
2024-07-30 16:39:54 +1000

    Added View Controller

---
+++ Magic Recorder/Home/EditScreenVC.swift
@@ -0,0 +1,29 @@
+//
+//  EditScreenVC.swift
+//  Magic Recorder
+//
+//  Created by Pranish Pathak on 30/7/2024.
+//
+
+import UIKit
+
+class EditScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
